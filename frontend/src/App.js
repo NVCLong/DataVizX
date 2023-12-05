cd19301896f0ce24cfd9f191691a362454fa15fc
Nguyen Duy <thanhduy1706@gmail.com>
2023-12-05 15:39:31 +0700

    update login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,18 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Form from "./components/Form";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex w-full h-screen">
+        <div className="w-full flex items-center justify-center lg:w-1/2">
+          <Form />
+        </div>
+
+        <div className="hidden relative lg:flex h-full w-1/2 items-center justify-center bg-gray-200">
+          <div className="w-60 h-60 bg-gradient-to-tr from-violet-500 to bg-pink-500 rounded-full animate-spin"></div>
+          <div className="w-full h-1/2 absolute bottom-0 bg-white/10 backdrop-blur-lg"></div>
+        </div>
+
     </div>
   );
 }
-
 export default App;
