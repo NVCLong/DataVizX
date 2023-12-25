d3b9c373efcbdecb40f4b5e24703794a18271968
Long Ngo <126145913+NVCLong@users.noreply.github.com>
2023-12-25 11:30:50 +0700

    Update chartlist api

--- backend/controllers/siteController.js
+++ backend/controllers/siteController.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const chartList= require("../models/chartlist.model")
 const User = require("../models/user.model")
+const Collection= require("../models/collection.model")
 const mongooseHelper= require("../untils/mongooseHelper")
 
 class SiteController {
@@ -12,10 +13,18 @@ class SiteController {
     // [GET] /charlist
     async chartListPage(req, res) {
         try{
-           await chartList.find({userId: req.cookies.userId})
-               .then(function(results){
+           await chartList.findOne({userId: req.cookies.userId})
+               .then(async function(results){
+                   const user= await User.findOne({_id: req.cookies.userId});
                    const lists= results;
-                   res.json({ success:true, chartlist: lists  })
+                   const listCollection= lists.DataList
+                   let userCollection= []
+                   for ( let collection of listCollection) {
+                       console.log(typeof  collection)
+                       let element = await Collection.findById(collection._id);
+                       userCollection.push(element)
+                   }
+                   res.json({ success:true, chartlist: lists, user: user , collection: userCollection  })
                })
                .catch(function(err){
                    console.log(err)
