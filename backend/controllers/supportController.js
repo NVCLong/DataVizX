d672aa59d17e5d58b672434b1ad7f257c526ddcb
Long Ngo <126145913+NVCLong@users.noreply.github.com>
2024-01-06 23:15:47 +0700

    create the support feature and delete collection route

--- backend/controllers/supportController.js
+++ backend/controllers/supportController.js
@@ -5,15 +5,16 @@ const openai= new OpenAI({
     apiKey: process.env.API
 });
 const supportController= {
-
+    // support/post     (post the question and get the answer from the server with the openai support)
     async getRequest(req, res){
         try{
+            console.log(typeof req.body.question)
             const  response= await openai.chat.completions.create({
                 model: 'gpt-3.5-turbo',
-                message:[{"role": "user", "content": req.body.question}],
+                messages:[{"role": "user", "content": req.body.question}],
                 max_tokens: 100
             })
-            console.log(response)
+            res.status(200).json({response: response.choices[0].message})
         }catch(e){
             console.log(e)
         }
